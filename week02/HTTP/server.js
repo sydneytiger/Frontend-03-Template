5bcb86bd5e67e36e1d1bb9a129195422e9bc0426
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-08-10 12:06:39 +1000

    implemented HTTP request and response for toy-browser

--- week02/HTTP/server.js
+++ week02/HTTP/server.js
@@ -1,19 +1,22 @@
 const http = require('http');
 
+const port = 8008;
+
 http.createServer((request, response) => {
   let body = [];
   request.on('error', err => {
     console.log(err);
   })
   .on('data', chunk => {
-    body.push(Buffer.from(chunk));
+    body.push(chunk);
+    // body.push(Buffer.from(chunk));
   })
   .on('end', () => {
     body = Buffer.concat(body).toString();
     console.log('body:', body);
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('Hello World \n');
+    response.end('<html><body>Hello Tiger Chen</body></html> \n');
   });
-}).listen(8088);
+}).listen(port);
 
-console.log("server started");
+console.log("server started listen on port:", port);
